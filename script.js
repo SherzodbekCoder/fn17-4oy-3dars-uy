ded3b087066b9a7d0819ebed741477dd52da2652
asosiymenu007 <asosiymenu007@gmail.com>
2024-01-17 11:59:09 +0500

    js misol

---
+++ script.js
@@ -0,0 +1,60 @@
+// 1-misol
+// 3 ta son berilgan bular orasidan 3 ga karralilarning kopaytmasini topuvchi algoritm yozing.
+
+/**
+let son1 = 12;
+let son2 = 9;
+let son3 = 6;
+
+if (son1 % 3 == 0) {
+    console.log(`Birinchi ozgaruvchi:`, son1);
+} else {
+
+} if (son2 % 3 == 0) {
+    console.log(`Ikkinchi ozgaruvchi:`, son2);
+
+} if (son3 % 3 == 0) {
+    console.log(`Uchinchi ozgaruvchi:`, son3);
+} */
+
+
+
+//2-misol
+//3 xonali son berilgan uning raqamlar yigindisini topadigan algoritm yozing.
+
+/**
+let num = 444;
+let son1 = num % 10;
+let son2 = Math.floor(num / 10) % 10;
+let son3 = Math.floor(num / 100);
+
+let sum = son1 + son2 + son3;
+
+console.log("3 xonali sonning raqamlari yig'indisi: " + sum);
+*/
+
+
+//3-misol
+// 3 xonali son berilgan uning toq raqamlari kopaytmasini topuvchi algoritm yozing.
+
+// let son = 749;
+// let ozgaruvchi = 0;
+
+// if (son % 2 === 1) {
+//     ozgaruvchi = (son % 10);
+
+// } else {
+
+// } if (son % 2 === 1) {
+//     ozgaruvchi *= (son % 10);
+//     son = (son - son % 10) / 10;
+
+// } 
+//oxiriga yetme qoldi uztoz boshim ogrib qoldi tushunme ketti qolganini/////////////////
+
+
+
+
+
+//4-misol
+let 
